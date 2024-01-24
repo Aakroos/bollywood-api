935256322f085eb01d141b01dc3808fe879c1889
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-01-24 16:56:40 +0530

    chore: :wrench: add cors

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,25 @@ import express from "express";
 import { logger } from "@/utils/logger";
 import { morganConfig } from "@/middlewares/morgan";
 import { apiHandler } from "@/utils/api-handler";
+import cors from "cors";
+import { getOrigins } from "@/utils/get-origins";
+import cookieParser from "cookie-parser";
 
 export const startServer = () => {
 	const app = express();
 
 	const PORT = process.env["PORT"];
+	const WHITELISTED_ORIGINS_STRING =
+		process.env["WHITELISTED_ORIGINS"] ?? "http://localhost:8000";
+
+	app.use(
+		cors({
+			credentials: true,
+			origin: getOrigins(WHITELISTED_ORIGINS_STRING),
+		}),
+	);
+	app.use(express.json());
+	app.use(cookieParser());
 
 	app.use(morganConfig);
 
